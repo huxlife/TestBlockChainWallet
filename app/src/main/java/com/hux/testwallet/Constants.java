6ef5f947397d2011acc0b46036387e12d2277ca3
hangxu <tsu321@163.com>
2025-10-15 18:08:06 +0800

    remove

--- app/src/main/java/com/hux/testwallet/Constants.java
+++ app/src/main/java/com/hux/testwallet/Constants.java
@@ -11,8 +11,8 @@ public class Constants {
     public static final String LIA_ADDRESS = "0x6a6708Fd8f1C064Ad66fAc165F8E8895f09de131";
 
    /**MAINNET请求地址**/
-    public static final String ETHEREUM_MAINNET_URL="https://mainnet.infura.io/v3/2a6baa8e4be94d63a7bbe8ffcd4b17fc";
+    public static final String ETHEREUM_MAINNET_URL="https://mainnet.infura.io/v3/";
     /**SEPOLIA请求地址**/
-    public static final String ETHEREUM_SEPOLIA_URL="https://sepolia.infura.io/v3/2a6baa8e4be94d63a7bbe8ffcd4b17fc";
+    public static final String ETHEREUM_SEPOLIA_URL="https://sepolia.infura.io/v3/";
 
 }
